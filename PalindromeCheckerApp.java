2fa56f92c359411cb90bb034db846e8e083b89be
nv0496 <nirmalvivek95@gmail.com>
2026-02-27 14:46:12 +0530

    [Nirmal Vivek] Added usecase1 palindromechecker

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp{
+ public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Version: 1.0");
+        System.out.println("System initialized Succesfully");
+}}
